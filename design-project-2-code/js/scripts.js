46146db10615e9a7ab04200f6498433c0cfe85c8
Student <example@example.com>
2018-06-07 11:22:32 -0700

    add sass media queries, utilize @content

---
+++ design-project-2-code/js/scripts.js
@@ -0,0 +1,6 @@
+function splashClick() {
+  let clickWrapper = document.getElementById('wrapper');
+  let clickSplash = document.getElementById('splash');
+  clickSplash.style.display = 'none';
+  clickWrapper.style.display = 'block'
+}
