c7b70d583468df29fec02c853061a37d23db9f23
Student <example@example.com>
2018-06-08 11:27:16 -0700

    add javascript hide/show functions for all links to immitate page change

--- design-project-2-code/js/scripts.js
+++ design-project-2-code/js/scripts.js
@@ -4,3 +4,45 @@ function splashClick() {
   clickSplash.style.display = 'none';
   clickWrapper.style.display = 'block'
 }
+
+function  amenitiesClick() {
+  let clickAmenities = document.getElementById('hideSite');
+  let clickAmenitiesShowing = document.getElementById('amenitiesShowing');
+  let linkFlavorBanner = document.getElementById('link-flavor-banner');
+  let linkButton = document.getElementById('link-button');
+
+  clickAmenities.style.display = 'none';
+  clickAmenitiesShowing.style.display = 'block';
+  linkButton.style.backgroundImage = 'url(imgs/amenitiesbutton.svg)';
+  linkFlavorBanner.style.backgroundImage = 'url(imgs/shakehands.svg)';
+}
+
+function  activitiesClick() {
+  let clickAmenities = document.getElementById('hideSite');
+  let clickAmenitiesShowing = document.getElementById('amenitiesShowing');
+  let linkFlavorBanner = document.getElementById('link-flavor-banner');
+  let linkButton = document.getElementById('link-button');
+  linkButton.style.backgroundImage = 'url(imgs/activitiesbutton.svg)';
+  linkFlavorBanner.style.backgroundImage = 'url(imgs/drawinghands.svg)';
+  clickAmenities.style.display = 'none';
+  clickAmenitiesShowing.style.display = 'block';
+}
+
+function ourspaceClick() {
+  let clickAmenities = document.getElementById('hideSite');
+  let clickAmenitiesShowing = document.getElementById('amenitiesShowing');
+  let linkFlavorBanner = document.getElementById('link-flavor-banner');
+  let linkButton = document.getElementById('link-button');
+  linkButton.style.backgroundImage = 'url(imgs/ourspacebutton.svg)'
+  linkFlavorBanner.style.backgroundImage = 'url(imgs/fistgroup.svg)';
+  clickAmenities.style.display = 'none';
+  clickAmenitiesShowing.style.display = 'block';
+
+}
+
+function homeClick() {
+  let clickAmenities = document.getElementById('hideSite');
+  let clickAmenitiesShowing = document.getElementById('amenitiesShowing');
+  clickAmenities.style.display = 'block';
+  clickAmenitiesShowing.style.display = 'none';
+}
